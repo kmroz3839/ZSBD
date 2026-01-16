23f1a1fdc40c8de618703928437996d81d930651
counter185 <33550839+counter185@users.noreply.github.com>
2026-01-16 11:08:02 +0100

    *

---
+++ projekt/init_tables.sql
@@ -0,0 +1,100 @@
+CREATE TABLE `customers` (
+    `customer_id` INT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(100) UNIQUE NOT NULL
+);
+
+CREATE TABLE `employees` (
+    `employee_id` INT PRIMARY KEY,
+    `name` VARCHAR(100) NOT NULL,
+    `position` VARCHAR(100) NOT NULL,
+    `hire_date` DATE NOT NULL
+);
+
+CREATE TABLE `games` (
+    `serial` VARCHAR(32) NOT NULL,
+    `title` TEXT NOT NULL,
+    `platform` TEXT NOT NULL,
+    PRIMARY KEY (`serial`)
+);
+
+CREATE TABLE `inventory` (
+    `game_id` VARCHAR(32) PRIMARY KEY,
+    `stock` INT NOT NULL,
+    FOREIGN KEY (`game_id`) REFERENCES `games`(`serial`)
+);
+
+CREATE TABLE `sales` (
+    `sale_id` INT PRIMARY KEY,
+    `game_id` VARCHAR(32) NOT NULL,
+    `sale_date` DATE NOT NULL,
+    `quantity` INT NOT NULL,
+    `price` DECIMAL(10, 2) NOT NULL,
+    `customer_id` INT,
+    `employee_id` INT,
+    FOREIGN KEY (`employee_id`) REFERENCES `employees`(`employee_id`),
+    FOREIGN KEY (`customer_id`) REFERENCES `customers`(`customer_id`),
+    FOREIGN KEY (`game_id`) REFERENCES `games`(`serial`)
+);
+
+--sprawdź czy jest wystarczająco dużo w magazynie
+CREATE OR REPLACE PROCEDURE validate_sale (
+    p_game_id IN VARCHAR2,
+    p_quantity IN INT
+) AS
+    v_stock INT;
+BEGIN
+    SELECT stock INTO v_stock FROM inventory WHERE game_id = p_game_id;
+    IF v_stock < p_quantity THEN
+        RAISE_APPLICATION_ERROR(-20001, 'brak w magazynie: ' || p_game_id);
+    END IF;
+END;
+/
+
+--trigger przed dodaniem sprzedaży
+CREATE SEQUENCE sale_seq START WITH 1;
+CREATE OR REPLACE TRIGGER sale_trigger
+BEFORE INSERT ON sales
+FOR EACH ROW
+BEGIN
+    validate_sale(:NEW.game_id, :NEW.quantity);
+    --auto increment sale_id
+    SELECT sale_seq.NEXTVAL INTO :NEW.sale_id FROM dual;
+    decrement_inventory(:NEW.game_id, :NEW.quantity);
+END;
+/
+
+CREATE OR REPLACE PROCEDURE decrement_inventory (
+    p_game_id IN VARCHAR2,
+    p_quantity IN INT
+) AS
+BEGIN
+    UPDATE inventory
+    SET stock = stock - p_quantity
+    WHERE game_id = p_game_id;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE update_inventory (
+    p_game_id IN VARCHAR2,
+    p_quantity IN INT
+) AS
+BEGIN
+    UPDATE inventory
+    SET stock = p_quantity
+    WHERE game_id = p_game_id;
+END;
+/
+
+CREATE OR REPLACE PROCEDURE sale_now (
+    p_game_id IN VARCHAR2,
+    p_quantity IN INT,
+    p_price IN DECIMAL,
+    p_customer_id IN INT,
+    p_employee_id IN INT
+) AS
+BEGIN
+    INSERT INTO sales (game_id, sale_date, quantity, price, customer_id, employee_id)
+    VALUES (p_game_id, SYSDATE, p_quantity, p_price, p_customer_id, p_employee_id);
+END;
+/
---
+++ projekt/load_from_redump.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+baseurl = "http://redump.org"
+scan_urls = [
+    "/discs/system/ps3/region/Eu",
+    "/discs/system/psp/region/Eu/"
+]
+
+entries = []
+pages_urls = []
+
+for url in scan_urls:
+    response = requests.get(baseurl + url)
+    html = response.text
+    soup = bs(html, "html.parser")
+    pages_div = soup.find("div", {"class": "pages"})
+    pages_links = pages_div.find_all("a")
+    pages_urls += [x.get('href') for x in pages_links]
+    pages_urls.insert(0, url)
+
+for page in pages_urls:
+    print(f"processing: {page}...")
+    response = requests.get(baseurl + page)
+    html = response.text
+    soup = bs(html, "html.parser")
+
+    table = soup.find("table", {"class": "games"})
+    rows = table.find_all("tr")
+    for row in rows:
+        tds = row.find_all("td")
+        if len(tds) >= 6:
+            editions = tds[4].text.strip()
+            title_a = tds[1].find("a")
+            title_span = title_a.find("span", {"class": "small"})
+            title = title_span.text.strip() if title_span is not None else title_a.text.strip()
+            for edition in editions.split(", "):
+                entry = {
+                    "title": title + f" ({edition})",
+                    "platform": tds[2].text.strip(), 
+                    "serial": tds[6].get('title').strip() if tds[6].get('title') is not None else tds[6].text.strip(),
+                }
+                #print(entry)
+                entries.append(entry)
+
+sql_file = "games.sql"
+with open(sql_file, "w", encoding="utf-8") as f:
+    f.write("""
+CREATE TABLE IF NOT EXISTS `games` (
+    `serial` VARCHAR(32) NOT NULL,
+    `title` TEXT NOT NULL,
+    `platform` TEXT NOT NULL,
+    PRIMARY KEY (`serial`)
+)
+            """)
+    for entry in entries:
+        f.write(f"""
+MERGE INTO `games` (`serial`, `title`, `platform`) VALUES (
+    '{entry['serial'].replace("'", "''")}',
+    '{entry['title'].replace("'", "''")}',
+    '{entry['platform'].replace("'", "''")}'
+);
+            """)

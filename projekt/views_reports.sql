3c833ba71435fee20a42a07feeecc81986197c1f
counter185 <33550839+counter185@users.noreply.github.com>
2026-01-23 07:44:05 +0100

    Update views_reports.sql

--- projekt/views_reports.sql
+++ projekt/views_reports.sql
@@ -23,6 +23,15 @@ FROM sales s
 GROUP BY TO_CHAR(s.sale_date, 'YYYY-"Q"Q')
 ORDER BY sale_quarter;
 
+CREATE OR REPLACE VIEW v_yearly_sales AS
+SELECT
+    TO_CHAR(s.sale_date, 'YYYY') AS sale_year,
+    SUM(s.quantity) AS total_quantity,
+    SUM(s.price * s.quantity) AS total_revenue
+FROM sales s
+GROUP BY TO_CHAR(s.sale_date, 'YYYY')
+ORDER BY sale_year;
+
 CREATE OR REPLACE VIEW v_employee_sales AS
 SELECT
     e.employee_id,
@@ -43,6 +52,7 @@ CREATE TABLE sales_report (
 CREATE OR REPLACE PACKAGE gen_reports AS
     PROCEDURE generate_monthly_sales_report;
     PROCEDURE generate_quarterly_sales_report;
+    PROCEDURE generate_yearly_sales_report;
 END gen_reports;
 /
 CREATE OR REPLACE PACKAGE BODY gen_reports AS
@@ -69,5 +79,17 @@ CREATE OR REPLACE PACKAGE BODY gen_reports AS
         FROM v_quarterly_sales
         ORDER BY period;
     END generate_quarterly_sales_report;
+
+    PROCEDURE generate_yearly_sales_report IS
+    BEGIN
+        DELETE FROM sales_report;
+        INSERT INTO sales_report (period, total_quantity, total_revenue)
+        SELECT
+            sale_year AS period,
+            total_quantity,
+            total_revenue
+        FROM v_yearly_sales
+        ORDER BY period;
+    END generate_yearly_sales_report;
 
 END gen_reports;
